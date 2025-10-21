2edc94b28b394865e17e310e68c6a8a47adb7cda
Wiktor Stankiewicz <wiktorstankiewicz24@gmail.com>
2025-10-22 00:03:34 +0200

    chore: logs

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,4 @@
 FROM eclipse-temurin:21-jre
 WORKDIR /app
 COPY target/hydrogarden-backend.jar app.jar
-VOLUME /app/logs
-ENTRYPOINT ["java", "-jar", "app.jar"]
+ENTRYPOINT ["java", "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005", "-jar", "app.jar"]
--- src/main/java/com/hydrogarden/HydrogardenBackendApplication.java
+++ src/main/java/com/hydrogarden/HydrogardenBackendApplication.java
@@ -2,7 +2,13 @@ package com.hydrogarden;
 
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.embedded.EmbeddedWebServerFactoryCustomizerAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.embedded.TomcatWebServerFactoryCustomizer;
+import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.ServletWebServerFactoryAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -15,6 +21,8 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableConfigurationProperties
 @ConfigurationPropertiesScan
 @EnableWebSecurity
+@ImportAutoConfiguration({WebMvcAutoConfiguration.class, DispatcherServletAutoConfiguration.class,
+        ServletWebServerFactoryAutoConfiguration.class,EmbeddedWebServerFactoryCustomizerAutoConfiguration.class})
 public class HydrogardenBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(HydrogardenBackendApplication.class, args);
